c941ac50d2153f00f2fa8fddd9131e01900db590
ShortyN1969 <november.snp90@gmail.com>
2020-04-20 18:48:37 +0900

    Q004完了

--- src/main/java/q004/Q004.java
+++ src/main/java/q004/Q004.java
@@ -5,11 +5,34 @@ package q004;
  * <p>
  * ListManagerクラスをnewして、小さい順に並び変えた上でcheckResult()を呼び出してください。
  * <p>
- * 実装イメージ: ListManager data = new ListManager(); // TODO 並び換え data.checkResult();
+ * 実装イメージ: ListManager data = new ListManager();
  * <p>
  * - ListManagerクラスを修正してはいけません - ListManagerクラスの dataList を直接変更してはいけません - ListManagerクラスの比較 compare
  * と入れ替え exchange を使って実現してください
  */
 public class Q004 {
+
+    private static final int ARR_SIZE = 100;
+
+    public static void main(String[] args) {
+        ListManager listManager = new ListManager();
+
+        sort(listManager, 0).checkResult();
+    }
+
+    private static ListManager sort(ListManager listManager, int recentlySortedIndex) {
+        for (int pivot = ARR_SIZE - 1; pivot > recentlySortedIndex; pivot--) {
+            if (listManager.compare(pivot - 1, pivot) == 1) {
+                listManager.exchange(pivot - 1, pivot);
+            }
+        }
+        recentlySortedIndex++;
+
+        if (recentlySortedIndex == ARR_SIZE - 1) {
+            return listManager;
+        }
+        return sort(listManager, recentlySortedIndex);
+    }
+
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 6分
