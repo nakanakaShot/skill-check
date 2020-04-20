9fd0d70de813ce374cb8da29e50a3327b0a86eb9
ShortyN1969 <november.snp90@gmail.com>
2020-04-20 18:48:09 +0900

    Q004フォーマッタ適用

--- src/main/java/q004/Q004.java
+++ src/main/java/q004/Q004.java
@@ -2,17 +2,13 @@ package q004;
 
 /**
  * Q004 ソートアルゴリズム
- *
+ * <p>
  * ListManagerクラスをnewして、小さい順に並び変えた上でcheckResult()を呼び出してください。
- *
- * 実装イメージ:
- * ListManager data = new ListManager();
- * // TODO 並び換え
- * data.checkResult();
- *
- * - ListManagerクラスを修正してはいけません
- * - ListManagerクラスの dataList を直接変更してはいけません
- * - ListManagerクラスの比較 compare と入れ替え exchange を使って実現してください
+ * <p>
+ * 実装イメージ: ListManager data = new ListManager(); // TODO 並び換え data.checkResult();
+ * <p>
+ * - ListManagerクラスを修正してはいけません - ListManagerクラスの dataList を直接変更してはいけません - ListManagerクラスの比較 compare
+ * と入れ替え exchange を使って実現してください
  */
 public class Q004 {
 }
