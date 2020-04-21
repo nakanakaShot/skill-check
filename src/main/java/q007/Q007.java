ee0c06cecf3162dbc7f0ca7ed1d47d597690022f
ShortyN1969 <november.snp90@gmail.com>
2020-04-21 10:32:46 +0900

    Q007フォーマッタ適用

--- src/main/java/q007/Q007.java
+++ src/main/java/q007/Q007.java
@@ -1,6 +1,7 @@
 package q007;
 
 /**
+ * <pre>
  * q007 最短経路探索
  *
  * 壁を 'X' 通路を ' ' 開始を 'S' ゴールを 'E' で表現された迷路で、最短経路を通った場合に
@@ -11,20 +12,23 @@ package q007;
  * 迷路データは MazeInputStream から取得してください。
  * 迷路の横幅と高さは毎回異なりますが、必ず長方形（あるいは正方形）となっており、外壁は全て'X'で埋まっています。
  * 空行が迷路データの終了です。
+ * </pre>
  *
-
-[迷路の例]
-XXXXXXXXX
-XSX    EX
-X XXX X X
-X   X X X
-X X XXX X
-X X     X
-XXXXXXXXX
-
-[答え]
-14
+ * <pre>
+ * [迷路の例]
+ * XXXXXXXXX
+ * XSX    EX
+ * X XXX X X
+ * X   X X X
+ * X X XXX X
+ * X X     X
+ * XXXXXXXXX
+ *
+ * [答え]
+ * 14
+ * </pre>
  */
 public class Q007 {
+
 }
 // 完成までの時間: xx時間 xx分
