e601ea96226b739f08b54a13837733f5fbc53729
ShortyN1969 <november.snp90@gmail.com>
2020-04-22 12:48:39 +0900

    Q007 迷路探索、迷路探索結果クラスを作成

---
+++ src/main/java/q007/MazeResolver.java
@@ -0,0 +1,11 @@
+package q007;
+
+import q007.model.MazeResult;
+
+public class MazeResolver {
+
+    static MazeResult resolve() {
+
+        return new MazeResult();
+    }
+}
---
+++ src/main/java/q007/model/MazeResult.java
@@ -0,0 +1,5 @@
+package q007.model;
+
+public class MazeResult {
+
+}
