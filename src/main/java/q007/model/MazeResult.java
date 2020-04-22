1408c237208d9284f2ea9a4c63fa5f5acc9d4e13
ShortyN1969 <november.snp90@gmail.com>
2020-04-22 16:16:52 +0900

    Q007 探索結果クラスを作成

--- src/main/java/q007/model/MazeResult.java
+++ src/main/java/q007/model/MazeResult.java
@@ -2,4 +2,13 @@ package q007.model;
 
 public class MazeResult {
 
+    private final int steps;
+
+    public MazeResult(int steps) {
+        this.steps = steps;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
 }
