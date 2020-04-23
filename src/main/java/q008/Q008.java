285db271e39c3f65013a16eac38c50df1b8c80b5
ShortyN1969 <november.snp90@gmail.com>
2020-04-23 10:17:23 +0900

    Q008 フォーマッタ適用

--- src/main/java/q008/Q008.java
+++ src/main/java/q008/Q008.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 /**
+ * <pre>
  * Q008 埋め込み文字列の抽出
  *
  * 一般に、定数定義の場合を除いて、プログラム中に埋め込み文字列を記述するのは良くないとされています。
@@ -17,24 +18,28 @@ import java.util.stream.Stream;
  * - 実行ディレクトリ配下（再帰的にチェック）に存在するすべてのjavaファイルをチェックする
  * - ファイル名はディレクトリ付きでも無しでも良い
  * - 行の内容を出力する際は、先頭のインデントは削除しても残しても良い
-
-[出力イメージ]
-Q001.java(12): return "test";  // テストデータ
-Q002.java(10): private static final DATA = "test"
-Q002.java(11): + "aaa";
-Q002.java(20): if (test == 'x') {
-Q004.java(13): Pattern pattern = Pattern.compile("(\".*\")|(\'.*\')");
-
+ * </pre>
+ *
+ * <pre>
+ * [出力イメージ]
+ * Q001.java(12): return "test";  // テストデータ
+ * Q002.java(10): private static final DATA = "test"
+ * Q002.java(11): + "aaa";
+ * Q002.java(20): if (test == 'x') {
+ * Q004.java(13): Pattern pattern = Pattern.compile("(\".*\")|(\'.*\')");
+ * </pre>
  */
 public class Q008 {
+
     /**
      * JavaファイルのStreamを作成する
      *
-     * @return
-     * @throws IOException
+     * @return JavaファイルのStream
+     * @throws IOException 入出力例外
      */
     private static Stream<File> listJavaFiles() throws IOException {
-        return Files.walk(Paths.get(".")).map(Path::toFile).filter(f -> f.getName().endsWith(".java"));
+        return Files.walk(Paths.get(".")).map(Path::toFile)
+            .filter(f -> f.getName().endsWith(".java"));
     }
 }
 // 完成までの時間: xx時間 xx分
