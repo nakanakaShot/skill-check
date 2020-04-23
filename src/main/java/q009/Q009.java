2d5951da46ac50ebe7253592f0b0c09eae15f071
ShortyN1969 <november.snp90@gmail.com>
2020-04-23 19:24:16 +0900

    Q009 完了

--- src/main/java/q009/Q009.java
+++ src/main/java/q009/Q009.java
@@ -1,5 +1,15 @@
 package q009;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
 /**
  * <pre>
  * Q009 重い処理を別スレッドで実行
@@ -27,5 +37,83 @@ package q009;
  */
 public class Q009 {
 
+    public static void main(String[] args) {
+
+        Map<BigDecimal, CompletableFuture> executors = new HashMap<>();
+
+        while (true) {
+            Optional<BigDecimal> input = readIntegerFromStd();
+
+            if (!input.isPresent()) {
+                printStatus(executors);
+                continue;
+            }
+
+            if (input.get().equals(BigDecimal.ZERO)) {
+                if (confirmExit()) {
+                    break;
+                }
+                continue;
+            }
+
+            executors.put(input.get(), awaitPrimeFactorize(input.get()));
+        }
+        System.out.println("終了中...");
+        executors.values().forEach(CompletableFuture::join);
+
+        System.out.println("実行結果");
+        printStatus(executors);
+    }
+
+    private static CompletableFuture<Set<BigDecimal>> awaitPrimeFactorize(BigDecimal target) {
+        return CompletableFuture
+            .supplyAsync(() -> PrimeFactorize.factorize(target))
+            .thenApply(bigDecimals -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return bigDecimals;
+            });
+    }
+
+    private static void printStatus(Map<BigDecimal, CompletableFuture> map) {
+        map.entrySet().stream().sorted(Comparator.comparing(Entry::getKey))
+            .forEach(entry -> printStatus(entry.getKey(), entry.getValue()));
+    }
+
+    private static void printStatus(BigDecimal value, CompletableFuture cf) {
+        String status = cf.isDone() ? cf.join().toString() : "実行中";
+        System.out.println(value + ": " + status);
+    }
+
+    private static Optional<BigDecimal> readIntegerFromStd() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("■素因数分解する整数を入力してください。(整数: 処理実行, 0: 終了)");
+
+        String input = sc.nextLine();
+
+        if (input.isEmpty()) {
+            return Optional.empty();
+        }
+
+        try {
+            return Optional.of(
+                BigDecimal.valueOf(Long.parseLong(input))
+            );
+        } catch (NumberFormatException ex) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static boolean confirmExit() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("処理を終了しますか？(y/N)");
+        String input = sc.nextLine();
+
+        return "y".equals(input.toLowerCase());
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 1時間 20分
