a8055ce30df53042cee213ccb9466547b8228750
ShortyN1969 <november.snp90@gmail.com>
2020-04-25 12:42:54 +0900

    Q005 基準 秒 -> 分

--- src/main/java/q005/model/WorkTime.java
+++ src/main/java/q005/model/WorkTime.java
@@ -2,8 +2,7 @@ package q005.model;
 
 public class WorkTime {
 
-    private static final int HOUR_AS_SECONDS = 3600;
-    private static final int MINUTE_AS_SECONDS = 60;
+    private static final int HOUR_AS_MINUTES = 60;
 
     private int workTime;
 
@@ -11,13 +10,13 @@ public class WorkTime {
         this.workTime = workTime;
     }
 
-    int getAsSeconds(){
+    int getAsSeconds() {
         return this.workTime;
     }
 
-    String getFormattedWorkTime(){
-        int hours = this.workTime / HOUR_AS_SECONDS;
-        int minutes = this.workTime % MINUTE_AS_SECONDS;
+    String getFormattedWorkTime() {
+        int hours = this.workTime / HOUR_AS_MINUTES;
+        int minutes = this.workTime % HOUR_AS_MINUTES;
         return String.format("%02d時間%02d分", hours, minutes);
     }
 }
